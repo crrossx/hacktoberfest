876b1d123e8dac7ffc0b110f3e211040fd0fc8f8
Deepak Kumar <64521665+itsdeepak-kumar@users.noreply.github.com>
2021-10-02 12:41:15 +0530

    Update Wasey.cpp

--- Wasey.cpp
+++ Wasey.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 cout << "I am a hunter, she wants to see my gun and when I pull it out woman starts to run!!"
+cout<<"hey hunter's can you please add me in to your team i also want to hunt......"
 
 return 0;
 }
