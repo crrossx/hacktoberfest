73add719a98c581e5cf3d5fd4881c5840b415853
crossover2 <>
2021-10-18 14:36:05 +0000

    completed the song

--- womaniya.cpp
+++ womaniya.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
 cout << "Oh wonamiya aa aa womaniya"
-  cout<<"kurta pajama kala kala kala......"
+  cout<<"kurta pajama kala kala kala deepak ki gaand me mota chala ......"
 return 0;
 }
